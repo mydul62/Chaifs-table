e12d758ae0810ba799241c7928100aa0d7ffece4
unknown <mydulcse62.niter@gmail.com>
2024-03-15 22:47:52 +0600

    added card add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ import RecipeItems from "./component/recipe/RecipeItems";
 import { useEffect, useState } from "react";
 function App() {
   const [data,setData]=useState([]);
+  const [cardData,setCardData]=useState([]);
   useEffect(()=>{
     fetch('data.json')
     .then(res=>res.json())
     .then(data=>setData(data));
   },[])
 
+  const handleCardAdd=(info)=>{
+    let newData = [...cardData,info]
+    setCardData(newData)
+  }
   return (
     <>
     <MainHeader></MainHeader>
     <HeroSection></HeroSection>
     <RecipeTitle></RecipeTitle>
-    <RecipeItems recipeData={data}></RecipeItems>
+    <RecipeItems cardData={cardData} cardAdd={handleCardAdd} recipeData={data}></RecipeItems>
     </>
   )
 }
---
+++ src/component/recipe/recipeItem/RecipeItem.jsx
@@ -0,0 +1,37 @@
+
+const RecipeItem = ({item,cardAdd}) => {
+  return (
+      <div className="p-6 border border-[#28282833] col-span-3 bg-base-100 shadow-xl ">
+              <figure className=" ">
+                <img className="rounded-lg" src={item.recipe_image} alt="Shoes" />
+              </figure>
+              <div className=" space-y-4">
+               <div className=" py-4 space-y-3">
+               <h2 className="card-title text-xl text-[#282828] font-semibold">{item.recipe_name}</h2>
+                <p className=" text-sm text-[#878787]">{item.short_description}</p>
+               </div>
+                <hr />
+                <div>
+                  <h2 className=" font-medium text-[18px] text-[#282828]">Ingredients:{item.ingredients.length}</h2>
+                  <ul className=" list-disc">
+                    {item.ingredients.slice(0,3).map((ing,index) => (
+                      <li className=" text-[18px] text-[#878787]" key={index}>{ing}</li>
+                    ))}
+                  </ul>
+                </div>
+                <hr />
+                <div className=" flex gap-5">
+                  <div className="text-sm text-[#282828CC]">{item.preparing_time} minutes</div>
+                  <div className="text-sm text-[#282828CC]">{item.calories} calories</div>
+                </div>
+                <div>
+                  <button onClick={()=>cardAdd(item)} className=" py-[13px] px-6 bg-[#0BE58A] rounded-full">
+                    Want to Cook
+                  </button>
+                </div>
+              </div>
+            </div>
+  );
+};
+
+export default RecipeItem;
